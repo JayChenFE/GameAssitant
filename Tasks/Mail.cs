0ee58889d8dba1da011be53f1b5af22e8e696351
jaychenfe <chenjiehwy@163.com>
2025-01-07 14:31:41 +0800

    修改收取邮件等待时间

--- Tasks/Mail.cs
+++ Tasks/Mail.cs
@@ -15,13 +15,12 @@ namespace GameAssistant
 
         protected override void DoTask()
         {
-            ImageAction.FindAndClickImages("邮件", "一键领取", "空白", "删除已读", "确定");
-            
+            ImageAction.FindAndClickImages(2, "邮件", "一键领取", "空白", "删除已读", "确定");
+
             ImageAction.FindAndClickImage("系统邮件");
             for (int i = 0; i < 3; i++)
             {
-                ImageAction.FindAndClickImages("删除已读", "确定", "时间", "领取", "空白");
-                Thread.Sleep(500);
+                ImageAction.FindAndClickImages(2, "删除已读", "确定", "时间", "领取", "空白");
             }
 
             ImageAction.FindAndClickImage("关闭");
